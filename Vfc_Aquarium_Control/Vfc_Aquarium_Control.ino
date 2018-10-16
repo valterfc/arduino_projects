c1c3f77bdca68a534d6adf080098575927288a2b
ValterFC <valter@accellog.com>
2018-10-16 19:06:38 -0300

    Initial commit

---
+++ Vfc_Aquarium_Control/Vfc_Aquarium_Control.ino
@@ -0,0 +1,51 @@
+#include "ESP8266WiFi.h"
+#include "ESP8266WebServer.h"
+#include "config.h"
+#include "page_index.h"
+
+ESP8266WebServer server(80);
+
+
+void setup() {
+  //BUILTIN_LED = GPIO2 DO ESP8266
+  pinMode(BUILTIN_LED, OUTPUT);  //DEFINE O PINO COMO SAÍDA
+
+  Serial.begin(74880);
+  WiFi.begin(ssid, password);  //Connect to the WiFi network
+ 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+    delay(500);
+    Serial.println("Waiting to connect…");
+  }
+ 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //Print the local IP
+ 
+  server.on("/other", []() {   //Define the handling function for the path
+    server.send(200, "text / plain", "Other URL");
+  });
+ 
+  server.on("/", handleRootPath);    //Associate the handler function to the path
+  server.begin();                    //Start the server
+  Serial.println("Server listening");
+
+  digitalWrite(BUILTIN_LED, HIGH); //LED DA PLACA APAGA (ACIONAMENTO COM SINAL LÓGICO INVERSO PARA O PINO 2)
+  delay(1000); //INTERVALO DE 1 SEGUNDO
+}
+ 
+void loop() {
+  digitalWrite(BUILTIN_LED, LOW); //LED DA PLACA ACENDE (ACIONAMENTO COM SINAL LÓGICO INVERSO PARA O PINO 2)
+  delay(1000); //INTERVALO DE 1 SEGUNDO
+
+  server.handleClient();         //Handling of incoming requests
+
+  //Serial.println("teste");
+
+  digitalWrite(BUILTIN_LED, HIGH); //LED DA PLACA APAGA (ACIONAMENTO COM SINAL LÓGICO INVERSO PARA O PINO 2)
+  delay(1000); //INTERVALO DE 1 SEGUNDO
+}
+ 
+void handleRootPath() {            //Handler for the rooth path
+  //server.send(200, "text/plain", "Hello world");
+  printPageIndex();
+}
---
+++ Vfc_Aquarium_Control/config.example.h
@@ -0,0 +1,3 @@
+//copy this file to "config.h"
+const char* ssid = "ssid_to_connect";
+const char* password = "ssid_password";
