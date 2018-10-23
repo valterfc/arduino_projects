997715b894dca0c3b7e727f006dd480b01a12565
ValterFC <valter@accellog.com>
2018-10-22 21:54:43 -0300

    Start Quasar Project to make PWA page

---
+++ Vfc_AquaControl/make_pwa/src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-select
+      stack-label="Language"
+      :options="[
+        { label: 'Brazilian portuguese', value: 'pt-br' },
+        { label: 'English (US)',         value: 'en-us' }
+      ]"
+      v-model="lang"
+    />
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
